4c6dcc7947ba9d66acdbdc0c10fe1d2a7d7863a3
lutfiix77-commits <lutfiix77@gmail.com>
2026-02-07 03:59:17 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="c4-coins",
+    version="0.0.1",
+    author="C4-Coins Team",
+    packages=find_packages(),
+    # Hanya masukkan library yang BUKAN bawaan Python di sini
+    install_requires=[
+        'requests',
+        'emoji',
+    ],
+    entry_points={
+        'console_scripts': [
+            'c4-coins=c4_coins.main:main',
+        ],
+    },
+)
