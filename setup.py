767583cf14cf8196ffc1d4b5ead8a7298fe0f7dd
lutfiix77-commits <lutfiix77@gmail.com>
2026-02-07 14:34:37 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="c4-coins",
-    version="1.0.4",
+    version="1.0.5",
     author="C4-Coins Team",
     packages=find_packages(),
     # Hanya masukkan library yang BUKAN bawaan Python di sini
